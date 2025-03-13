4d3673201231a4739aa18998680fa6560ed204b5
Stephen Balboa <stjosephbalboa@gmail.com>
2025-03-13 16:14:42 +0800

    0

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Print the formatted output
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
